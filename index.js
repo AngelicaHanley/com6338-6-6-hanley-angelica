74c086a324bf79410ade92b7ab0ec1b86c82c48b
Angelica Hanley <angelicahanley2@gmail.com>
2026-09-16 16:28:45 -0400

    fixed structure bugs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var questionsArr = [
   },
   {
     question: 'How many Stanley Cups have the Florida Panthers won?',
-    answer: '2',
+    answer: '2 cups',
     options: [
       '1 cup',
       '2 cups',
@@ -56,12 +56,26 @@ var score = 0
 var gameStart = false
 var btns = null
 var index = 0
+var timerId = null
+var quizContainer = document.querySelector('#quiz')
+var question = document.createElement('p')
+var optionsContainer = document.createElement('div')
+var answer1 = document.createElement('button')
+var answer2 = document.createElement('button')
+var answer3 = document.createElement('button')
+var answer4 = document.createElement('button')
+var timerText = document.createElement('p')
 
 //creating button on screen
 //if the user has never played the game before, the game should display a "start quiz" button
-
+if(localStorage.getItem('previous-score')){
+  var previousScore = localStorage.getItem('previous-score')
+  var scoreText = document.createElement('p')
+  quizContainer.appendChild(scoreText)
+  scoreText.textContent="Previous Score: "+ previousScore+"%"
+}
 var startBtn = document.createElement('button')
-document.body.appendChild(startBtn)
+quizContainer.appendChild(startBtn)
 startBtn.setAttribute('id','start-quiz')
 startBtn.textContent = "Start Quiz!"
 //If the user has taken the quiz before, the app should display the previous score
@@ -69,29 +83,16 @@ startBtn.textContent = "Start Quiz!"
 //setting screen to 1st question when start button is clicked
 startBtn.addEventListener('click', function(){
   gameStart=true
-  document.body.removeChild(startBtn)
-  //game container
-  var gameContainer = document.createElement('div')
-  document.body.appendChild(gameContainer)
-  gameContainer.classList.add('quiz-example')
-
-  //questionContainer
-  var questionContainer = document.createElement('div')
-  gameContainer.appendChild(questionContainer)
+  quizContainer.removeChild(startBtn)
+  if(localStorage.getItem('previous-score')){
+  quizContainer.removeChild(scoreText)}
 
   //question 
-  var question = document.createElement('p')
-  questionContainer.appendChild(question)
+  quizContainer.appendChild(question)
   console.log(questionsArr[0].question)
   
   //options
-  var optionsContainer = document.createElement('div')
-  questionContainer.appendChild(optionsContainer)
-  optionsContainer.setAttribute('id','quiz')
-  var answer1 = document.createElement('button')
-  var answer2 = document.createElement('button')
-  var answer3 = document.createElement('button')
-  var answer4 = document.createElement('button')
+  quizContainer.appendChild(optionsContainer)
   optionsContainer.appendChild(answer1)
   optionsContainer.appendChild(answer2)
   optionsContainer.appendChild(answer3)
@@ -104,8 +105,12 @@ startBtn.addEventListener('click', function(){
   answer3.textContent = questionsArr[0].options[2]
   answer4.textContent = questionsArr[0].options[3]
 
+  //timer
+  quizContainer.appendChild(timerText)
+  startTimer(29)
+
   //getting all buttons (answer choice options)
-  btns = document.querySelectorAll('#quiz > button')
+  btns = optionsContainer.querySelectorAll('button')
 
   //checking when an answer button is clicked
   for(var i=0; i<btns.length;i++){
@@ -114,26 +119,58 @@ startBtn.addEventListener('click', function(){
       console.log("clickedButton: "+ this.textContent)
       //getting current index of button, need later for correct answer stuff
       var buttonIndex = Array.from(btns).indexOf(this)
-      
-      //if curr question index is NOT last question, change to next question
-      if(index<questionsArr.length-1){
-        //update text content to next question
-        index++
-        question.textContent = questionsArr[index].question
-        answer1.textContent = questionsArr[index].options[0]
-        answer2.textContent = questionsArr[index].options[1]
-        answer3.textContent = questionsArr[index].options[2]
-        answer4.textContent = questionsArr[index].options[3] 
-      } else{
-        //endgame functionality
-        gameStart = false
-        console.log("END GAME")
+      if(this.textContent === questionsArr[index].answer){
+        score++
+        console.log("score: "+ score)
       }
+      //updates screen to next question
+      nextQuestion()
     })
   }
-
 })
 
+//timer function (for each question)
+function startTimer(seconds) {
+  timerText.textContent = "30"
+  clearInterval(timerId)
+  timerId = setInterval(function(){ 
+    if(seconds === 0){
+      clearInterval(timerId)
+      nextQuestion()
+    }
+    else{
+     timerText.textContent = seconds
+    }
+    seconds--
+  }, 1000)
+}
 
-
-  
+function nextQuestion(){
+  //if curr question index is NOT last question, change to next question
+  if(index<questionsArr.length-1){
+    //update text content to next question
+    index++
+    startTimer(29)
+    question.textContent = questionsArr[index].question
+    answer1.textContent = questionsArr[index].options[0]
+    answer2.textContent = questionsArr[index].options[1]
+    answer3.textContent = questionsArr[index].options[2]
+    answer4.textContent = questionsArr[index].options[3] 
+  } else{
+    //endgame functionality
+    clearInterval(timerId)
+    gameStart = false
+    console.log("END GAME")
+    quizContainer.removeChild(question)
+    quizContainer.removeChild(optionsContainer)
+    quizContainer.removeChild(timerText)
+    quizContainer.appendChild(scoreText)
+    //updating score
+    score = Math.round(((score/questionsArr.length)*100))
+    localStorage.setItem('previous-score', score)
+    scoreText.textContent="Previous Score: "+ score+"%"
+    score=0
+    index=0
+    quizContainer.appendChild(startBtn)
+  }
+}
